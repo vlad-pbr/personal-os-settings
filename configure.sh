61317e767a67f20063d68ca22cb1119cf0e0b5f8
Vlad Poberezhny <vlad.pbr@gmail.com>
2021-03-16 20:53:33 +0200

    some separation

--- configure.sh
+++ configure.sh
@@ -11,21 +11,31 @@ declare -A STAGES=( \
 	sudo apt-get update
 	sudo apt-get upgrade" \
 	\
-	["2. Install software"]="\
-	sudo add-apt-repository multiverse
-	sudo apt-get update
-	sudo apt-get install software-properties-common apt-transport-https gdebi-core wget
+	["2. Install utils"]="\
+	sudo apt-get install curl git python3-pip software-properties-common apt-transport-https gdebi-core wget
+	"\
+	\
+	["3. VSCode"]="\
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	sudo add-apt-repository \"deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main\"
-	sudo add-apt-repository \"deb http://repository.spotify.com stable non-free\"
-	wget -O- https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-	sudo apt-get install curl git python3-pip code steam spotify-client
+	sudo apt-get install code" \
+	\
+	["4. Steam"]="\
+	sudo add-apt-repository multiverse
+	sudo apt-get update
+	sudo apt-get install steam"\
+	\
+	["5. Discord"]="\
 	wget -O /tmp/discord.deb \"https://discordapp.com/api/download?platform=linux&format=deb\"
 	sudo gdebi /tmp/discord.deb
-	rm -f /tmp/discord.deb
-	" \
+	rm -f /tmp/discord.deb"\
 	\
-	["3. Various system configurations"]="\
+	["6. Spotify"]="\
+	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+	sudo add-apt-repository \"deb http://repository.spotify.com stable non-free\"
+	sudo apt-get install spotify-client"\
+	\
+	["7. Various system configurations"]="\
 	echo Disabling mouse acceleration...
 	gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 	
@@ -33,11 +43,11 @@ declare -A STAGES=( \
 	git config --global user.email 'vlad.pbr@gmail.com'
 	git config --global user.name 'Vlad Poberezhny'" \
 	\
-	["4. Install latest Nvidia driver"]="\
+	["8. Install latest Nvidia driver"]="\
 	sudo ubuntu-drivers install
 	sudo apt-get install libnvidia-gl-\$(ubuntu-drivers list 2> /dev/null | egrep -o '^nvidia-driver-[0-9]*' | cut -d- -f 3 | sort | tail -n1):i386" \
 	\
-	["5. Get kiwi"]="\
+	["9. Get kiwi"]="\
 	sudo python3 -m pip install --upgrade pip
 	mkdir -p $HOME/.local/bin
 	curl https://raw.githubusercontent.com/vlad-pbr/kiwi/master/kiwi > $HOME/.local/bin/kiwi
